f2108f7ed060752005fcab15d217848cd759f23e
Ysmael Braga <ysmaelbraga01@gmail.com>
2020-08-13 19:20:29 -0300

    feito a parte do login

---
+++ src/components/LoginCriar.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Login criar</p>
+</template>
+
+<script>
+export default {
+  name: "LoginCriar",
+};
+</script>
+
+<style></style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,7 +4,10 @@
       <router-link to="/" class="logo">
         <img src="@/assets/ranek.svg" alt="Ranek" />
       </router-link>
-      <router-link class="btn" to="login">Vender / Login</router-link>
+      <router-link v-if="$store.state.login" class="btn" to="/usuario">{{
+        nome
+      }}</router-link>
+      <router-link v-else class="btn" to="/login">Vender / Login</router-link>
     </nav>
   </header>
 </template>
@@ -12,6 +15,11 @@
 <script>
 export default {
   name: "TheHeader",
+  computed: {
+    nome() {
+      return this.$store.state.usuario.nome.replace(/ (.* )/, " ");
+    },
+  },
 };
 </script>
 
@@ -23,9 +31,11 @@ nav {
   padding: 15px 20px;
   box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
 }
+
 .logo {
   padding: 10px 0;
 }
+
 .logo img {
   width: 90px;
 }
